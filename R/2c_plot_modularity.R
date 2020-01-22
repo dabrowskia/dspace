48eab0b19d16a481f3300f5bd40812b7b388f935
Nowosad <tupiszakaczki@gmail.com>
2020-01-22 10:23:04 +0100

    adds functions specification

--- R/2c_plot_modularity.R
+++ R/2c_plot_modularity.R
@@ -8,7 +8,7 @@
 #' @export
 #' @examples 
 #' data("quakes")
-#' quakes <- SpatialPointsDataFrame(cbind( quakes$lat, quakes$long), quakes)
+#' quakes <- sp::SpatialPointsDataFrame(cbind( quakes$lat, quakes$long), quakes)
 #' quakes.modularity <- find_no_clusters(quakes, polygon = FALSE, data = 3:4)
 #' plot_modularity(quakes.modularity)
 
@@ -16,10 +16,10 @@ plot_modularity <- function(mod)
 {
   mod %>%
     as.data.frame() %>%
-    ggplot(aes(x = as.numeric(names(mod)), y = mod)) +
-    geom_point() +
-    geom_vline(xintercept = which(mod == max(mod)) + 1) +
-    xlab("number of clusters") +
-    ylab("Modularity") +
-    theme_light()
+    ggplot2::ggplot(ggplot2::aes(x = as.numeric(names(mod)), y = mod)) +
+    ggplot2::geom_point() +
+    ggplot2::geom_vline(xintercept = which(mod == max(mod)) + 1) +
+    ggplot2::xlab("number of clusters") +
+    ggplot2::ylab("Modularity") +
+    ggplot2::theme_light()
 }
